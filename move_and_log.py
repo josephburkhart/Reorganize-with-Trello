54549c5d06aae84d54d3cfc9f00a36a82a578b74
Joseph Burkhart <joseph.burkhart1@comcast.net>
2022-03-14 11:16:40 -0700

    Add trailing slash syntax for directories in print statements within move_and_log

--- move_and_log.py
+++ move_and_log.py
@@ -32,12 +32,18 @@ def move(source: Path, destination: Path, base_dir: Path):
     printing messages to the console as needed'''
     # Check if the named file/directory exists
     if not source.exists():
-        print(f"Warning: {source.name} does not exist in {source.parent}{os.sep}\nMove has been skipped. Continuing...")
+        if source.is_dir():
+            print(f"Warning: {os.sep}{source.name}{os.sep} does not exist in {source.parent}{os.sep}\nMove has been skipped. Continuing...")
+        else:
+            print(f"Warning: {source.name} does not exist in {source.parent}{os.sep}\nMove has been skipped. Continuing...")
         return
     
     # Check if there is a duplicate file/directory at the destination
     if destination.exists():
-        print("Warning: f{source.name} already exists in {destination.parent}{os.sep}\nMove has been skipped. Continuing...")
+        if source.is_dir():
+            print(f"Warning: {os.sep}{source.name}{os.sep} already exists in {destination.parent}{os.sep}\nMove has been skipped. Continuing...")
+        else:
+            print(f"Warning: {source.name} already exists in {destination.parent}{os.sep}\nMove has been skipped. Continuing...")
         return
     
     # Create destination folder if necessary    TODO: modify print statement to include base reorg directory
@@ -51,7 +57,7 @@ def move(source: Path, destination: Path, base_dir: Path):
         #destination.mkdir(parents=False, exist_ok=False)    
         copy_tree(str(source), str(destination), preserve_times=True)
         shutil.rmtree(source)
-        print(f"{source.name} has been moved to .{os.sep}{shorten_path(destination.parent, base_dir)}{os.sep}")
+        print(f"{os.sep}{source.name}{os.sep} has been moved to .{os.sep}{shorten_path(destination.parent, base_dir)}{os.sep}")
     
     # If name is a file, move it with shutil.move
     else:
@@ -64,22 +70,31 @@ def move_message(source: Path, destination: Path, base_dir: Path):
     Note that table_entry must have the following attributes: name'''
     short_source = shorten_path(source, base_dir)
     short_dest = shorten_path(destination, base_dir)
-    move_msg = f"moved {source.name} in {short_source.parent} to {short_dest.parent}\n"
+    if source.is_dir():
+        move_msg = f"moved {os.sep}{source.name}{os.sep} in {short_source.parent}{os.sep} to {short_dest.parent}{os.sep}\n"
+    else:
+        move_msg = f"moved {source.name} in {short_source.parent}{os.sep} to {short_dest.parent}{os.sep}\n"
     return move_msg
 
 def error_message(table_entry, current_dir: Path, base_dir: Path):
     '''Compose a message describing the movement error
     Note that table_entry must have the following attributes: name, flag'''
-    print(f"Issue found at {current_dir / table_entry.name}") #better if this were a data structure
+    source = current_dir / table_entry.name
+    if source.is_dir():
+        print(f"Issue found at {current_dir / table_entry.name}{os.sep}")
+    else:
+        print(f"Issue found at {current_dir / table_entry.name}")
     
     # Determine the type of issue
     issues = {'d': 'Duplicate', 'u': 'Unclear'}
     issue_type = issues.get(table_entry.flag, 'Issue') #returns 'Issue' if flag is not 'd' or 'u'
     
     # Compose error message
-    source = current_dir / table_entry.name
     short_source_parent = shorten_path(source.parent, base_dir)
-    error_msg = issue_type + ": " + str(table_entry.name) + " in " + str(short_source_parent)
+    if source.is_dir():
+        error_msg = f"{issue_type}: {os.sep}{table_entry.name}{os.sep} in .{os.sep}{short_source_parent}{os.sep}"
+    else:
+        error_msg = f"{issue_type}: {table_entry.name} in .{os.sep}{short_source_parent}{os.sep}"
     return error_msg
 
 def log_message(log_file_path: Path, time, message):
